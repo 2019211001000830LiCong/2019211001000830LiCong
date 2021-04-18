842fd23e644ed043a085a05e6f9ab0c28b8c59aa
LiCong <858211645@qq.com>
2021-04-18 19:48:10 +0800

    HomeWork7

--- src/main/java/com/LiCong/week5/LoginServlet.java
+++ src/main/java/com/LiCong/week5/LoginServlet.java
@@ -1,5 +1,7 @@
 package com.LiCong.week5;
 
+import com.LiCong.dao.UserDao;
+import com.LiCong.model.User;
 import com.mysql.cj.protocol.Resultset;
 
 import javax.servlet.ServletException;
@@ -15,40 +17,37 @@ import java.sql.SQLException;
 @WebServlet(name = "LoginServlet",value = "/login")
 public class LoginServlet extends HttpServlet {
     Connection con = null;
-
     @Override
     public void init() throws ServletException {
-        super.init();
+
         con = (Connection) getServletContext().getAttribute("con");
     }
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        PrintWriter out = response.getWriter();
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        String sql = "select id,username,password,email,gender,birthdate from usertable where username='";
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        PrintWriter out=response.getWriter();
+
+        UserDao userDao = new UserDao();
         try {
-            Resultset rs = (Resultset) con.createStatement().executeQuery(sql);
-            if (rs.next()) {
-                request.setAttribute("id", rs.getInt("id"));
-                request.setAttribute("username", rs.getString("username"));
-                request.setAttribute("password", rs.getString("password"));
-                request.setAttribute("email", rs.getString("email"));
-                request.setAttribute("gender", rs.getString("gender"));
-                request.setAttribute("birthdate", rs.getString("birthdate"));
-                request.getRequestDispatcher("userlist.jsp").forward(request, response);
-            } else {
-                request.setAttribute("message", "Username or Password Error!!!");
-                request.getRequestDispatcher("login.jsp").forward(request, response);
+            User user= userDao.findByUsernamePassword(con,username,password);
+            if(user!=null) {
+                request.setAttribute("user",user);
+                request.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(request,response);
             }
+            else {
+                request.setAttribute("message","Username or Password Error!!!");
+                request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request,response);
+            }
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
     }
 }
